a04fb1bf09e496847a463f5f5992828aafb82e63
Simon <68133192+SaiMonYo@users.noreply.github.com>
2021-12-01 05:44:22 +0000

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,34 @@
+def part1(data):
+    t = 0
+    c = 0
+    # start on 1
+    for i in range(1, len(data)):
+        if data[i] > data[i-1]:
+            c += 1
+    return c
+
+def part2(data):
+    c = 0
+    old = 0
+    # sweeping window 
+    for i in range(2, len(data)):
+        if sum(data[i-2:i+1]) > old:
+            c += 1
+        old = sum(data[i-2:i+1])
+    # minus 1 for the first window as it is always > 0
+    # this cost me leaderboard position on part 2
+    return c - 1
+
+
+
+with open("Day1.txt") as file:
+    data = file.read().split("\n")
+    if data[-1] == "" or data[-1] == "\n":
+        data = data[:-1]
+    # designed code yesterday try except not needed
+    try:
+        data = list(map(int, data))
+    except:
+        pass
+    print(f"Part1 solution = {part1(data)}")
+    print(f"Part2 solution = {part2(data)}")
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Tasks
+
+| Day | Name                               | Part 1 | Part 1 time to finish (s) | Part 2 | Part 2 time to finish (s) | Points      | Combined runtime (ms) |
+| --- | :--------------------------------- |:------ |:------------------------- |:------ | :------------------------ | :-----------| :---------------------|
+| 1   | Sonar Sweep                        | 77th   | 			60 				|1748th  | 			 494             |     24      | 		  0.3		   |
+| 2   | XXX                      		   | XXXX   | 			XX 				|XXXXth  | 			 XXX             |     XX      | 		  XX		   |
+
+__Current score:__ 0 points
