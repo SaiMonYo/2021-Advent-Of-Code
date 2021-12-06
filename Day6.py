3aa988b22c7501d88c8100aa881dc3a0a0f034b4
Simon <68133192+SaiMonYo@users.noreply.github.com>
2021-12-06 08:47:14 +0000

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -1,46 +1,49 @@
 def part1(data):
-    for j in range(20):
+    for j in range(80):
+        # new fish created so that loop isnt affected
         new = []
         for i in range(len(data)):
+            # changing each one
             data[i] -= 1
+            # looping adding 1 new 8 fish
             if data[i] == -1:
                 data[i] = 6
                 new.append(8)
+        # combine lists
         data += new
     return len(data)
-        
 
 
 def part2(data):
     dic = {}
+    # dictionaty creation
     for n in data:
         if n not in dic:
             dic[n] = 1
         else:
             dic[n] += 1
     for i in range(256):
+        # changing keys
         dic1 = {(k-1): v for k, v in dic.items()}
+        # if we need to make new fish
         if -1 in dic1:
             x = dic1[-1]
+            # spawned
             dic1[8] = x
+            # fish looped but not spawned
             if 6 in dic1:
                 dic1[6] += x
             else:
                 dic1[6] = x
+            # removing negative fish
             del dic1[-1]
+        # changing main dictionary
         dic = dic1
+    # summing values
     return sum(dic.values())
-
-def test(data):
-    dic = {1: 6206821033, 2: 5617089148, 3: 5217223242, 4: 4726100874, 5: 4368232009, 6: 3989468462}
-    c = 0
-    for n in data:
-        c += dic[n]
-    return c
 
 with open("Day6.txt") as file:
     data = file.read().split(",")
     data = list(map(int, data))
     print(f"Part1 solution = {part1(data.copy())}")
     print(f"Part2 solution = {part2(data.copy())}")
-    print(f"Part1 solution = {test(data.copy())}")
